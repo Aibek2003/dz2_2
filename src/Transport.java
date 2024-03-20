2d881da12a9a891fac72cfa657042853df6a5b0d
Aibek2003 <raiymbekovaibek03@gmail.com>
2024-03-21 02:07:18 +0600

    перезагрузил

--- src/Transport.java
+++ src/Transport.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Transport {
+public abstract class Transport implements Printable {
+    private String type;
+    private String name;
+    private int speedLimit;
+    private int yearProduced;
+
+    public Transport(String type, String name, int speedLimit, int yearProduced) {
+        this.type = type;
+        this.name = name;
+        this.speedLimit = speedLimit;
+        this.yearProduced = yearProduced;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSpeedLimit() {
+        return speedLimit;
+    }
+
+    public int getYearProduced() {
+        return yearProduced;
+    }
+
+    @Override
+    public void print() {
+        System.out.println("Transport type: " + this.type +
+                "\nTransport name: " + this.name +
+                "\nTransport speed: " + this.speedLimit + " km/h" +
+                "\nTransport produced year: " + this.yearProduced);
+    }
 }
